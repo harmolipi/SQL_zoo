e7cb589b59ba3e74cfb2394ecaf203ad4142da1b
harmolipi <nikob381@gmail.com>
2021-08-10 17:17:35 -0400

    Added some questions

--- 7_more_join_operations.sql
+++ 7_more_join_operations.sql
@@ -1,14 +1,20 @@
--- 1. 
+-- 1. List the films where the yr is 1962 [Show id, title]
 
--- 2. 
 
--- 3. 
+-- 2. Give year of 'Citizen Kane'.
 
--- 4. 
 
--- 5. 
+-- 3. List all of the Star Trek movies, include the id, title and yr (all of these movies include the words Star Trek in the title). Order results by year.
 
--- 6. 
+
+-- 4. What id number does the actor 'Glenn Close' have?
+
+
+-- 5. What is the id of the film 'Casablanca'
+
+
+-- 6. Obtain the cast list for 'Casablanca'. Use movieid=11768, (or whatever value you got from the previous question)
+
 
 -- 7. 
 
