3602468b9ea77dbb9a275261fa3a9c67501c785d
harmolipi <nikob381@gmail.com>
2021-08-11 13:47:29 -0400

    Created blank answer sheet

---
+++ 9-_window_functions.sql
@@ -0,0 +1,17 @@
+-- 1. Show the lastName, party and votes for the constituency 'S14000024' in 2017.
+
+
+-- 2. Show the party and RANK for constituency S14000024 in 2017. List the output by party
+
+
+-- 3. Use PARTITION to show the ranking of each party in S14000021 in each year. Include yr, party, votes and ranking (the party with the most votes is 1).
+
+
+-- 4. Use PARTITION BY constituency to show the ranking of each party in Edinburgh in 2017. Order your results so the winners are shown first, then ordered by constituency.
+
+
+-- 5. Show the parties that won for each Edinburgh constituency in 2017.
+
+
+-- 6. Show how many seats for each party in Scotland in 2017.
+
