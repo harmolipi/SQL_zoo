a0611a1a97eeadb68c151c85bb692096c1b90587
harmolipi <nikob381@gmail.com>
2021-08-11 17:11:52 -0400

    Created blank answer sheet

---
+++ 9_self_join.sql
@@ -0,0 +1,29 @@
+-- 1. How many stops are in the database.
+
+
+-- 2. Find the id value for the stop 'Craiglockhart'
+
+
+-- 3. Give the id and the name for the stops on the '4' 'LRT' service.
+
+
+-- 4. Run the query and notice the two services that link these stops have a count of 2. Add a HAVING clause to restrict the output to these two routes.
+
+
+-- 5. Execute the self join shown and observe that b.stop gives all the places you can get to from Craiglockhart, without changing routes. Change the query so that it shows the services from Craiglockhart to London Road.
+
+
+-- 6. Change the query so that the services between 'Craiglockhart' and 'London Road' are shown. If you are tired of these places try 'Fairmilehead' against 'Tollcross'
+
+
+-- 7. Give a list of all the services which connect stops 115 and 137 ('Haymarket' and 'Leith')
+
+
+-- 8. Give a list of the services which connect the stops 'Craiglockhart' and 'Tollcross'
+
+
+-- 9. Give a distinct list of the stops which may be reached from 'Craiglockhart' by taking one bus, including 'Craiglockhart' itself, offered by the LRT company. Include the company and bus no. of the relevant services.
+
+
+-- 10. Find the routes involving two buses that can go from Craiglockhart to Lochend. Show the bus no. and company for the first bus, the name of the stop for the transfer, and the bus no. and company for the second bus.
+
