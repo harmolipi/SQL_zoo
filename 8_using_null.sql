ee801bb87d69407cf6ecf3c94d69f7e272dd4305
harmolipi <nikob381@gmail.com>
2021-08-11 10:20:03 -0400

    Completed exercises

--- 8_using_null.sql
+++ 8_using_null.sql
@@ -1,5 +1,5 @@
 -- 1. List the teachers who have NULL for their department.
-SELECT teacher.name
+SELECT name
 FROM teacher
 WHERE dept IS NULL;
 
@@ -28,13 +28,26 @@ FROM teacher
 LEFT JOIN dept ON teacher.dept=dept.id;
 
 -- 7. Use COUNT to show the number of teachers and the number of mobile phones.
-
+SELECT COUNT(name), COUNT(mobile)
+FROM teacher;
 
 -- 8. Use COUNT and GROUP BY dept.name to show each department and the number of staff. Use a RIGHT JOIN to ensure that the Engineering department is listed.
-
+SELECT dept.name, COUNT(teacher.id)
+FROM teacher
+RIGHT JOIN dept ON teacher.dept = dept.id
+GROUP BY dept.name;
 
 -- 9. Use CASE to show the name of each teacher followed by 'Sci' if the teacher is in dept 1 or 2 and 'Art' otherwise.
-
+SELECT name, CASE
+             WHEN dept IN (1, 2) THEN 'Sci'
+             ELSE 'Art'
+             END
+FROM teacher;
 
 -- 10. Use CASE to show the name of each teacher followed by 'Sci' if the teacher is in dept 1 or 2, show 'Art' if the teacher's dept is 3 and 'None' otherwise.
-
+SELECT name, CASE
+             WHEN dept IN (1, 2) THEN 'Sci'
+             WHEN dept = 3 THEN 'Art'
+             ELSE 'None'
+             END
+FROM teacher;
