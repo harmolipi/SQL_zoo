2269165fdfce6ca41840fa455cc09f451d377cd3
harmolipi <nikob381@gmail.com>
2021-08-11 14:48:49 -0400

    Created blank answer sheet

---
+++ 9+_covid_19.sql
@@ -0,0 +1,23 @@
+-- 1. Modify the query to show data from Spain
+
+
+-- 2. Modify the query to show confirmed for the day before.
+
+
+-- 3. Show the number of new cases for each day, for Italy, for March.
+
+
+-- 4. Show the number of new cases in Italy for each week - show Monday only.
+
+
+-- 5. Show the number of new cases in Italy for each week - show Monday only.
+
+
+-- 6. Include the ranking for the number of deaths in the table.
+
+
+-- 7. Show the infect rate ranking for each country. Only include countries with a population of at least 10 million.
+
+
+-- 8. For each country that has had at last 1000 new cases in a single day, show the date of the peak number of new cases.
+
