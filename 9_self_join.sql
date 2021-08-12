2e3c28bad216e4686a827b06a4df0c24fb63193d
harmolipi <nikob381@gmail.com>
2021-08-11 21:44:52 -0400

    Completed 1 and 2

--- 9_self_join.sql
+++ 9_self_join.sql
@@ -1,8 +1,11 @@
 -- 1. How many stops are in the database.
-
+SELECt count(id)
+from stops;
 
 -- 2. Find the id value for the stop 'Craiglockhart'
-
+SELECT id
+FROM stops
+WHERE name = 'Craiglockhart';
 
 -- 3. Give the id and the name for the stops on the '4' 'LRT' service.
 
